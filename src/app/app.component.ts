ddc025365b7a197f95f01155ed51657c0d40a3dd
Tomas Dangelo <tomidngl@gmail.com>
2023-03-30 22:13:05 -0300

    Ejercicio 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto-angular';
+
+  edad1 = '';
+  edad2 = '';
+  promedio: number = 0;
+  suma: number = 0;
+
+  calcular(){
+    this.suma = Number(this.edad1) + Number(this.edad2);
+    this.promedio = this.suma / 2;
+  }
+
+  limpiar(){
+
+  }
 }
